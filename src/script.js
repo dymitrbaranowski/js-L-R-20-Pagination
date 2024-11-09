06b6248cfe917efea2625bb5c3d07afa5b4aff93
Dymitr Baranowski <125156435+dymitrbaranowski@users.noreply.github.com>
2024-11-09 21:33:53 +0200

    fox4

--- src/script.js
+++ src/script.js
@@ -21,39 +21,126 @@
 //   );
 // }
 // getCharacter();
-const target = document.querySelector('.js-guard');
+// const target = document.querySelector('.js-guard');
 
+// let options = {
+//   root: null,
+//   rootMargin: '200px',
+//   threshold: 1.0,
+// };
+
+// let observer = new IntersectionObserver(onLoad, options);
+// function onLoad(evt) {
+//   console.log(evt);
+// }
+
+// const BASE_URL = 'https://api.themoviedb.org/3/';
+// const ENDPOINT = 'trending/movie/day';
+// const API_KEY = '345007f9ab440e5b86cef51be6397df1';
+// const list = document.querySelector('.js-list');
+// const loadMore = document.querySelector('.js-load');
+// let currentPage = 1;
+
+// loadMore.addEventListener('click', onLoad);
+
+// function onLoad() {
+//   currentPage += 1;
+//   getTrending(currentPage)
+//     .then(data => {
+//       list.insertAdjacentHTML('beforeend', createMarkup(data.results));
+
+//       if (data.page === data.total_pages) {
+//         loadMore.hidden = true;
+//       }
+//     })
+//     .catch(err => console.log(err));
+// }
+
+// function getTrending(page = 1) {
+//   return fetch(`${BASE_URL}${ENDPOINT}?api_key=${API_KEY}&page=${page}`).then(
+//     resp => {
+//       if (!resp.ok) {
+//         throw new Error(resp.statusText);
+//       }
+
+//       return resp.json();
+//     }
+//   );
+// }
+// getTrending()
+//   .then(data => {
+//     console.log(data);
+//     list.insertAdjacentHTML('beforeend', createMarkup(data.results));
+
+//     observer.observe(target);
+//     if (data.page !== data.total_pages) {
+//       loadMore.hidden = false;
+//     }
+//   })
+//   .catch(err => console.log(err));
+
+// function createMarkup(arr) {
+//   return arr
+//     .map(
+//       ({ poster_path, title }) => `<li>
+//     <img src="https://image.tmdb.org/t/p/w400${poster_path}" alt="${title}">
+//     <h2>${title}</h2>
+// </li>`
+//     )
+//     .join('');
+// }
+
+// // ПЕРЕРВА ДО 22.00
+
+// let counter = 0;
+// document.addEventListener('scroll', onScroll);
+
+// function onScroll() {
+//   counter += 1;
+//   console.log(counter);
+// }
+
+const target = document.querySelector('.js-guard');
+const BASE_URL = 'https://api.themoviedb.org/3/';
+const ENDPOINT = 'trending/movie/day';
+const API_KEY = '345007f9ab440e5b86cef51be6397df1';
+const list = document.querySelector('.js-list');
+
+let currentPage = 1;
 let options = {
   root: null,
   rootMargin: '200px',
   threshold: 1.0,
 };
 
-let observer = new IntersectionObserver(callback, options);
-function callback(evt) {
-  console.log(evt);
+let observer = new IntersectionObserver(onLoad, options);
+
+function onLoad(entries, observer) {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      currentPage += 1;
+      getTrending(currentPage)
+        .then(data => {
+          console.log(data);
+          list.insertAdjacentHTML('beforeend', createMarkup(data.results));
+          if (data.page === data.total_pages) {
+            observer.unobserve(target);
+          }
+        })
+        .catch(err => console.log(err));
+    }
+  });
 }
 
-const BASE_URL = 'https://api.themoviedb.org/3/';
-const ENDPOINT = 'trending/movie/day';
-const API_KEY = '345007f9ab440e5b86cef51be6397df1';
-const list = document.querySelector('.js-list');
-const loadMore = document.querySelector('.js-load');
-let currentPage = 1;
-
-loadMore.addEventListener('click', onLoad);
-
-function onLoad() {
-  currentPage += 1;
-  getTrending(currentPage)
-    .then(data => {
-      list.insertAdjacentHTML('beforeend', createMarkup(data.results));
-
-      if (data.page === data.total_pages) {
-        loadMore.hidden = true;
-      }
-    })
-    .catch(err => console.log(err));
+function createMarkup(arr) {
+  return arr
+    .map(
+      ({ poster_path, title }) => `<li>
+    <img src="https://image.tmdb.org/t/p/w400${poster_path}" alt="${title}">
+    <h2>${title}</h2>
+</li>`
+    )
+    .join('');
 }
 
 function getTrending(page = 1) {
@@ -73,32 +160,8 @@ getTrending()
     list.insertAdjacentHTML('beforeend', createMarkup(data.results));
 
     observer.observe(target);
-    if (data.page !== data.total_pages) {
-      loadMore.hidden = false;
-    }
   })
   .catch(err => console.log(err));
-
-function createMarkup(arr) {
-  return arr
-    .map(
-      ({ poster_path, title }) => `<li>
-    <img src="https://image.tmdb.org/t/p/w400${poster_path}" alt="${title}">
-    <h2>${title}</h2>
-</li>`
-    )
-    .join('');
-}
-
-// // ПЕРЕРВА ДО 22.00
-
-let counter = 0;
-document.addEventListener('scroll', onScroll);
-
-function onScroll() {
-  counter += 1;
-  console.log(counter);
-}
 
 // const BASE_URL = 'https://api.themoviedb.org/3/';
 // const ENDPOINT = 'trending/movie/day';
